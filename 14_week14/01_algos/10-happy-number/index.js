d3253d91e6d0c2274cc98da9a1575e9a7682c45c
Mendi03 <salva.mendis.03@gmail.com>
2025-10-10 10:21:55 -0500

    Final algo (complete)

--- 14_week14/01_algos/10-happy-number/index.js
+++ 14_week14/01_algos/10-happy-number/index.js
@@ -13,13 +13,43 @@
 function isHappy(n) {
   // TODO: Determine if n is a happy number
   // 1) Create a helper to compute sum of squares of digits
+  let sum = sumOfSquares(n);
   // 2) Initialize a Set for seen numbers
+  let seenNums = new Map();
   // 3) While n is not 1:
-  //      - Replace n with sum of squares of its digits
-  //      - If n already in seen → return false
-  //      - Add n to seen
+  while (n != 1) {
+    n = sumOfSquares(n);
+    if (seenNums.has(n)) {
+      // - Replace n with sum of squares of its digits
+      // - If n already in seen → return false
+      return false;
+    }
+    // - Add n to seen
+    seenNums.set(n, n);
+  }
   // 4) Return true if loop exits because n == 1
+  return true;
 }
+
+function sumOfSquares(n) {
+  let split = n.toString().split("").map(Number);
+  let result = 0;
+  for (let i = 0; i < split.length; i++) {
+    result += split[i] * split[i];
+  }
+  return result;
+}
+
+// 1) Create a helper to compute sum of squares of digits
+//   function sumOfSquares(num) {
+//     let sum = 0;
+//     while (num > 0) {
+//       let digit = num % 10;
+//       sum += digit * digit;
+//       num = Math.floor(num / 10);
+//     }
+//     return sum;
+//   } 
 
 function isHappyFloyd(n) {
   // TODO: Determine if n is a happy number using Floyd’s Cycle Detection
